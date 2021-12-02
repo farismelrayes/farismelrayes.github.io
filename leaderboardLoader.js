70511dfca5a55786d7c4f705ad76c481aa543376
Faris <9siraf@gmail.com>
2021-12-02 01:48:31 -0500

    Firebase imports update

--- leaderboardLoader.js
+++ leaderboardLoader.js
@@ -1,7 +1,7 @@
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js';
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-analytics.js';
 
 
 // Your web app's Firebase configuration
